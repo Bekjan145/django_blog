1669a3e6dafcd1a0a1074da64ec190dd89b1bafd
Bekjan145 <bekjigitovbekjan07@gmail.com>
2025-06-17 09:40:26 +0500

    dj

---
+++ blog/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+# Create your models here.
+
+
+from django.db import models
+
+
+class Blog(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+
+    def __str__(self):
+        return f"{self.title}"
